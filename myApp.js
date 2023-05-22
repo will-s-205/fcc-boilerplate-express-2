030730f463cdb6122830804d25f30c8e03f66aa3
will-s-205 <william.step.0205@gmail.com>
2023-05-22 04:04:52 +0000

    beautify the code

--- myApp.js
+++ myApp.js
@@ -4,21 +4,41 @@ let absolutePath = __dirname + '/views/index.html';
 let cssPath = __dirname + "/public";
 let bodyParser = require('body-parser');
 
+///////////////////////////////////////////////////
+
+// 12 Get Data from POST Requests // NOT WORKING
+// app.post("/name", function(req, res) {
+//   // Handle the data in the request
+//   var string = req.body.first + " " + req.body.last;
+//   res.json({ name: string });
+// });
+
+///////////////////////////////////////////////////
+
 //11 Use body-parser to Parse POST Requests
-app.get((req, res) => {
-  bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/name", function(req, res) {
+  // Handle the data in the request
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
 });
 
+///////////////////////////////////////////////////
+
 // 10 Get Query Parameter Input from the Client
-app.get('/name', (req, res) => {
-  const firstName = req.query.first;
-  const lastName = req.query.last;
-  // OR
-  // var { first: firstName, last: lastName } = req.query;
-  res.json({
-    name: `${firstName} ${lastName}`
-  });
-});
+// app.get('/name', (req, res) => {
+//   const firstName = req.query.first;
+//   const lastName = req.query.last;
+
+//   // OR
+//   // var { first: firstName, last: lastName } = req.query;
+//   res.json({
+//     name: `${firstName} ${lastName}`
+//   });
+// });
+
+///////////////////////////////////////////////////
 
 // 9 Get Route Parameter Input from the Client
 app.get('/:word/echo', (req, res) => {
@@ -27,6 +47,8 @@ app.get('/:word/echo', (req, res) => {
     echo: word
   });
 });
+
+///////////////////////////////////////////////////
 
 // 8 Chain Middleware to Create a Time Server
 // app.get("/now", (req, res, next) => {
@@ -51,6 +73,8 @@ app.get('/:word/echo', (req, res) => {
 // //   });
 // // });
 
+///////////////////////////////////////////////////
+
 // 7 Simple logger function
 // app.use((req, res, next) => {
 //   console.log(req.method + " " + req.path + " - " + req.ip);
@@ -63,6 +87,8 @@ app.get('/:word/echo', (req, res) => {
 //   console.log(string)
 //   next();
 // });
+
+///////////////////////////////////////////////////
 
 // 6 Allow to use ENV VAR
 app.get("/json", (req, res) => {
@@ -78,6 +104,8 @@ app.get("/json", (req, res) => {
   }
 });
 
+///////////////////////////////////////////////////
+
 // 5 Allow to use JSON
 app.get("/json", (req, res) => {
   res.json({
@@ -85,43 +113,30 @@ app.get("/json", (req, res) => {
   });
 });
 
+///////////////////////////////////////////////////
+
 // 4 Allow to use CSS
 app.use("/public", express.static(cssPath));
+
+///////////////////////////////////////////////////
 
 // 3 Allow to use index.html file
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 });
 
+///////////////////////////////////////////////////
+
 // 2 Allow to use browser for text
 app.get("/", function(req, res) {
   res.send("Hello Express");
 });
 
+///////////////////////////////////////////////////
+
 // 1 Allow to debug in console
 console.log("Hello world");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////////////////////
 
 module.exports = app;
